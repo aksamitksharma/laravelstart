2618ae87f6e3ee8b7e41bc91893099aaeabb26bf
amtiksharma <aksamtiksharma@gmail.com>
2021-07-14 11:21:51 +0530

    added web.php

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,29 @@
 <?php
 
-// class Facade{
-//     public static function __callStatic($name, $args){
-//         return app()->make(static::getFacadeAccessor())->$name();
-//     }
-// }
+class Facade{
+    public static function __callStatic($name, $args){
+        return app()->make(static::getFacadeAccessor())->$name();
+    }
+}
 
-// class Fish{
-//     public function swim(){
-//         return "swimming";
-//     }
-// }
+class Fish{
+    public function swim(){
+        return "swimming";
+    }
+}
 
-// app()->bind('fish', function(){
-//     return new Fish;
-// } );
+app()->bind('fish', function(){
+    return new Fish;
+} );
 
 
-// class FishFacade extends Facade{
-//     public static function getFacadeAccessor(){
-//         return 'fish';
-//     }
-// }
+class FishFacade extends Facade{
+    public static function getFacadeAccessor(){
+        return 'fish';
+    }
+}
 
-// dd(FishFacade::swim());
+dd(FishFacade::swim());
 
 
 // dd(app()->make('Game'));
